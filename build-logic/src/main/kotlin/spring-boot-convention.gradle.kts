d5471b951e4493f67db640b16427f21b43e6e2d8
jasper <2190320413@qq.com>
2026-08-26 12:10:37 +0800

    format

--- build-logic/src/main/kotlin/spring-boot-convention.gradle.kts
+++ build-logic/src/main/kotlin/spring-boot-convention.gradle.kts
@@ -4,9 +4,9 @@ plugins {
     id("org.springframework.boot")
 }
 
-    val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
 
-    dependencies {
-        implementation(platform(libs.findLibrary("spring-boot-dependencies").get()))
-        testImplementation(libs.findLibrary("spring-boot-starter-test").get())
-    }
+dependencies {
+    implementation(platform(libs.findLibrary("spring-boot-dependencies").get()))
+    testImplementation(libs.findLibrary("spring-boot-starter-test").get())
+}
