5e868c26059898c1d126bf4696c3b5868bb76d34
jasper <2190320413@qq.com>
2026-09-07 15:45:47 +0800

    spring aop jdk and cglib proxy

--- spring-lab/spring-aop/src/main/java/com/jasper/proxy/jdk/JdkProxyHandler.java
+++ spring-lab/spring-aop/src/main/java/com/jasper/proxy/jdk/JdkProxyHandler.java
@@ -9,6 +9,8 @@ import java.lang.reflect.Method;
  * InvocationHandler：调用处理器接口，代理对象的方法调用都会转发到该处理器的 invoke 方法中
  * Proxy：用于动态生成代理类和创建代理对象
  * 缺点：只能代理实现了接口的类
+ * 它在运行时，会动态生成一个实现了目标类所有接口的代理类
+ * 不直接执行目标方法，而是把调用转发给 InvocationHandler 的 invoke 方
  */
 @Slf4j
 public class JdkProxyHandler implements InvocationHandler {
