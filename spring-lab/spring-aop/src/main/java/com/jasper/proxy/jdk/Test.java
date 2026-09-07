a8b90052446d1000ea0d95b894193922306c5bd8
jasper <2190320413@qq.com>
2026-09-07 15:55:03 +0800

    spring aop jdk and cglib proxy

--- spring-lab/spring-aop/src/main/java/com/jasper/proxy/jdk/Test.java
+++ spring-lab/spring-aop/src/main/java/com/jasper/proxy/jdk/Test.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Proxy;
 
 public class Test {
     public static void main(String[] args) {
+        System.setProperty("jdk.proxy.ProxyGenerator.saveGeneratedFiles", "true");
         UserService target = new UserServiceImpl();
         // 生成代理对象
         UserService proxyInstance = (UserService) Proxy.newProxyInstance(
